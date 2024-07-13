35e06e121e63a66d17a06bbb47e7ef2bc8bccb53
pierrialexandervidmar <pierri@hotmail.com.br>
2024-07-13 15:04:15 -0300

    Criada classe Response

--- src/main/java/dev/pierri/EncurtaURL/Links/LinkResponse.java
+++ src/main/java/dev/pierri/EncurtaURL/Links/LinkResponse.java
@@ -10,5 +10,54 @@ public class LinkResponse {
     private String urlQrCode;
     private LocalDateTime urlCriadaEm;
 
+    public LinkResponse(Long id, String urlLong, String urlEncurtada, String urlQrCode, LocalDateTime urlCriadaEm) {
+        this.id = id;
+        this.urlLong = urlLong;
+        this.urlEncurtada = urlEncurtada;
+        this.urlQrCode = urlQrCode;
+        this.urlCriadaEm = urlCriadaEm;
+    }
 
+    public LinkResponse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrlLong() {
+        return urlLong;
+    }
+
+    public void setUrlLong(String urlLong) {
+        this.urlLong = urlLong;
+    }
+
+    public String getUrlEncurtada() {
+        return urlEncurtada;
+    }
+
+    public void setUrlEncurtada(String urlEncurtada) {
+        this.urlEncurtada = urlEncurtada;
+    }
+
+    public String getUrlQrCode() {
+        return urlQrCode;
+    }
+
+    public void setUrlQrCode(String urlQrCode) {
+        this.urlQrCode = urlQrCode;
+    }
+
+    public LocalDateTime getUrlCriadaEm() {
+        return urlCriadaEm;
+    }
+
+    public void setUrlCriadaEm(LocalDateTime urlCriadaEm) {
+        this.urlCriadaEm = urlCriadaEm;
+    }
 }
